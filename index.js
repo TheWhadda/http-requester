2a51d4a1639c2bd40bd4965ee9041b74c4d0f14b
Aleksandr Gurin <whaddaup@gmail.com>
2026-03-04 02:17:30 +0300

    Update aspect ratio to 2:3

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post("/generate", async (req, res) => {
         }],
         generation_config: {
           response_modalities: ["IMAGE"],
-          imageConfig: { aspectRatio: "413:600" }
+          imageConfig: { aspectRatio: "2:3" }
         }
       })
     }
